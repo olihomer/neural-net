26c4f313ed4520c5ba6c688f1d67d1bdc3b70cf2
Oli Homer <oliver.homer@btinternet.com>
2026-09-09 17:21:27 +0100

    Created Trainer class, cleaned up architecture

---
+++ neural net v1/Trainer.cpp
@@ -0,0 +1,8 @@
+//
+//  Trainer.cpp
+//  neural net v1
+//
+//  Created by Oliver Homer on 09/09/2026.
+//
+
+#include "Trainer.hpp"
---
+++ neural net v1/Trainer.hpp
@@ -0,0 +1,13 @@
+//
+//  Trainer.hpp
+//  neural net v1
+//
+//  Created by Oliver Homer on 09/09/2026.
+//
+
+#ifndef Trainer_hpp
+#define Trainer_hpp
+
+#include <stdio.h>
+
+#endif /* Trainer_hpp */
